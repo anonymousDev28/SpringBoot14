e776ee7d7534ab29ea69841a8ffd08e9eb528285
anonymousDev28 <92519101+anonymousDev28@users.noreply.github.com>
2023-02-16 14:43:55 +0700

    upload homework 16022023

---
+++ manager-rent-houses/manager-rent-houses/src/main/java/com/techmaster/managerrenthouses/ManagerRentHousesApplication.java
@@ -0,0 +1,13 @@
+package com.techmaster.managerrenthouses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerRentHousesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerRentHousesApplication.class, args);
+	}
+	
+}
---
+++ manager-rent-houses/manager-rent-houses/src/test/java/com/techmaster/managerrenthouses/ManagerRentHousesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techmaster.managerrenthouses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerRentHousesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
